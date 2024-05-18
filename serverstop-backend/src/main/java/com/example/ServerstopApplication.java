c45a668357ce2d2ee71ee37a799b022d0e296b79
catchme24 <roslyakov119@gmail.com>
2024-05-18 15:49:15 +0400

    changed config

--- serverstop-backend/src/main/java/com/example/ServerstopApplication.java
+++ serverstop-backend/src/main/java/com/example/ServerstopApplication.java
@@ -1,13 +1,18 @@
 package com.example;
 
 import org.hibernate.cfg.Environment;
+import org.springframework.aop.aspectj.SingletonAspectInstanceFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import com.example.security.SecurityProperty;
+import org.springframework.context.annotation.Scope;
 
 @SpringBootApplication
 @EnableConfigurationProperties({SecurityProperty.class})
+@ConfigurationProperties
 public class ServerstopApplication {
 
 	public static void main(String[] args) {
